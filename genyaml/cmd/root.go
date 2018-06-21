7c53d1bd0649140393370dbb280d83095dc6e21d
Chad Roberts <croberts@redhat.com>
2018-06-21 12:23:09 -0400

    Refactoring into a more suitable structure.

---
+++ genyaml/cmd/root.go
@@ -0,0 +1,107 @@
+/*
+Copyright 2017 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/ghodss/yaml"
+
+	"k8s.io/spark-on-k8s-operator/pkg/apis/sparkoperator.k8s.io/v1alpha1"
+)
+
+var defaultKubeConfig = os.Getenv("HOME") + "/.kube/config"
+
+var Namespace string
+var KubeConfig string
+
+var rootCmd = &cobra.Command{
+	Use:   "genyaml",
+	Short: "genyaml is the command-line tool for working with the Spark Operator",
+	Long: `genyaml is the command-line tool for working with the Spark Operator. It supports creating, deleting and 
+           checking status of SparkApplication objects. It also supports fetching application logs.`,
+}
+
+//func init() {
+//	rootCmd.PersistentFlags().StringVarP(&Namespace, "namespace", "n", "default",
+//		"The namespace in which the SparkApplication is to be created")
+//	rootCmd.PersistentFlags().StringVarP(&KubeConfig, "kubeconfig", "k", defaultKubeConfig,
+//		"The path to the local Kubernetes configuration file")
+//	rootCmd.AddCommand(createCmd, deleteCmd, statusCmd, logCommand, listCmd, forwardCmd)
+//}
+
+
+var data = `
+apiVersion: "sparkoperator.k8s.io/v1alpha1"
+kind: SparkApplication
+metadata:
+  name: spark-pi
+spec:
+  type: Scala
+  mode: cluster
+  image: "docker.io/crobby/openshift-spark:2.3"
+  mainClass: org.apache.spark.examples.SparkPi
+  mainApplicationFile: "local:///opt/spark/examples/jars/spark-examples_2.11-2.3.0.jar"
+  arguments:
+    - 1000
+  volumes:
+    - name: "test-volume"
+      hostPath:
+        path: "/tmp"
+        type: Directory
+  driver:
+    cores: 0.1
+    coreLimit: "200m"
+    memory: "512m"
+    labels:
+      version: 2.3.0
+    serviceAccount: spark
+    volumeMounts:
+      - name: "test-volume"
+        mountPath: "/tmp"
+  executor:
+    cores: 1
+    instances: 3
+    memory: "512m"
+    labels:
+      version: 2.3.0
+    volumeMounts:
+      - name: "test-volume"
+        mountPath: "/tmp"
+  restartPolicy: Never
+`
+
+
+
+func Execute() {
+	obj := v1alpha1.SparkApplication{}
+
+	err := yaml.Unmarshal([]byte(data), &obj)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("Text converted to object:\n%v\n\n", obj)
+
+	text, err := yaml.Marshal(&obj)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("Object converted to YAML:\n%s\n\n", string(text))
+}
